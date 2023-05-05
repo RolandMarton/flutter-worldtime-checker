07cd27277c59aaa7041886b0a8cd11b643c5cb8c
RolandMarton <roland111187@gmail.com>
2023-05-05 11:22:50 +0200

    Add retry package to solve API call issue, plus half of the project is finished. Initial overlay is done

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -1,31 +1,46 @@
 import 'package:http/http.dart';
 import 'dart:convert';
+import 'package:intl/intl.dart';
+import 'package:retry/retry.dart';
 
 class WorldTime {
-
   late String location; // location name for the UI
   late String time; // the time in that location
   late String flag; // url to an asset flag icon
   late String url; // location url for api endpoint
+  late bool isDaytime;
 
-  WorldTime({ required this.location, required this.flag, required this.url });
+  WorldTime({required this.location, required this.flag, required this.url});
 
   Future<void> getTime() async {
+    final client = Client();
+    request() => client.get(Uri.parse('http://worldtimeapi.org/api/timezone/$url'));
 
-    // make the request
-    Response response = await get(Uri.parse('http://worldtimeapi.org/api/timezone/$url'));
-    Map data = jsonDecode(response.body);
+    final response = await retry(
+      request,
+      maxAttempts: 5,
+      retryIf: (e) => e is Exception,
+      delayFactor: const Duration(seconds: 2),
+    );
+
+    if (response == null) {
+      time = 'Could not get time data';
+      return;
+    }
 
     // get properties from data
+    Map data = jsonDecode(response.body);
     String datetime = data['datetime'];
-    String offset = data['utc_offset'].substring(1,3);
+    String offset = data['utc_offset'].substring(1, 3);
 
     // create a DateTime object
     DateTime now = DateTime.parse(datetime);
     now = now.add(Duration(hours: int.parse(offset)));
 
     // set the time property
-    time = now.toString();
+    isDaytime = now.hour > 6 && now.hour < 20;
+    time = DateFormat.jm().format(now);
+
+    client.close();
   }
-  
 }
